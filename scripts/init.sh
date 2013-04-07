20d6626c1a1f51df2e31ba6043dc970a29353c3f
gandaman <github.10.gand@dfgh.net>
2013-04-07 15:36:56 +0200

    Improve robustness of init upon failures

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,18 +5,29 @@ if [ ! -d .git ] ; then
    exit 1
 fi
 
+fail() {
+   echo "**********************************************************************"
+   echo "Init step failed and continuing makes no sense. "
+   echo "If this was a failed clone of submodule, maybe retry the script again."
+   echo "If a module is consistently failing, try removing (rm -rf) the whole "
+   echo "submodule directory and retry the script.  If all else fails, do the "
+   echo "init steps manually (scripts/init.sh) for the other modules." 
+   echo "**********************************************************************"
+   exit 1
+}
+
 echo "*************************************************************"
 echo "Submodules"
 echo "*************************************************************"
 # Get the submodules
-git submodule init
-git submodule update
+git submodule init     || fail
+git submodule update   || fail
 
 echo "*************************************************************"
-echo "Itzam/C"
+echo "Fetch non-git modules"
 echo "*************************************************************"
 # Download ItzamC
-itzamc/get.sh
+itzamc/get.sh   || fail
 
 echo "*************************************************************"
 echo "Patches"
